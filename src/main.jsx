279c68547865fc411c3c6a47646b313b96cb7642
Judyahmed16 <Judyahmed166@gmail.com>
2026-02-21 13:30:30 +0200

    Sidebar works!!

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import ProductsPage from './pages/products'
 import { AddItemPage } from './pages/AddItemPage'
 import './index.css'
+import { Sidebar } from './components/Sidebar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <div className="flex h-screen bg-[#f6f8fa]">
+        <Sidebar />
         <main className="flex-1 overflow-y-auto p-8">
           <Routes>
             <Route path="/products" element={<ProductsPage />} />
